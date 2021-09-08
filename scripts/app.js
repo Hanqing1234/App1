4d40bbc5b192d8ae77aea400aade983f2a074f79
Hanqing1234 <lhq3361951@gmail.com>
2021-09-08 16:04:50 -0400

    Added nav link highlight

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,65 @@
 
 (function(){
 
-    function Start()
+    // function Start()
+    // {
+    //     console.log("App started");
+    // }
+
+    //declarative format
+    // let Start = function()
+    // {
+    //     console.log("App started");
+    // }
+
+    let Start = ()=>
     {
         console.log("App started");
+
+        let clickBtn = document.getElementById("clickBtn");
+
+        let a = document.getElementsByTagName("button");
+        console.log(a);
+
     }
+
+    let title = document.title;
+    title = title.toLowerCase();
+
+        let navAnchors = document.querySelectorAll("li a");
+        
+
+        for (const li of navAnchors) {
+            console.log(li);
+            let anchorString = li.getAttribute("href");
+            anchorString = anchorString.substr(0,anchorString.length - 5)
+            console.log(anchorString);
+            if(anchorString === title)
+            {
+                li.className = "nav-link active";
+            }
+            else if ((anchorString === "index") && (title === "home"))
+            {
+                li.className = "nav-link active";
+            }
+            
+        }
+    // //Nice, if I have id in each li
+    // switch (title) {
+    //     case "Home":
+
+    //         break;
+    //     case "Products":           
+    //         break;
+    //     case "About":           
+    //         break;
+    //     case "Services":           
+    //         break;    
+    //     case "Contact":           
+    //         break;    
+    //     default:
+    //         break;
+    // }
 
     window.addEventListener("load",Start);
 
